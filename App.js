95ee3db1a5085a7d7f1c6dc1351bec2d482394f6
deepdhar <dhar2017.slg@gmail.com>
2022-05-24 18:12:51 +0530

    updated SDK version

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, } from 'react-native';
 import Task from './components/Task';
 
